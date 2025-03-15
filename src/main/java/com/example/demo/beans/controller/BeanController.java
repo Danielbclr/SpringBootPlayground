c2c38cec8b23110e2d595ec6163f7177a19d5d07
Danielbclr <danielbclramos@gmail.com>
2025-03-14 22:08:45 -0300

    Implement Spring Boot Bean Demo
    Simple Demo to showcase different bean types in SpringBoot
    Added Support for Mobile

--- src/main/java/com/example/demo/beans/controller/BeanController.java
+++ src/main/java/com/example/demo/beans/controller/BeanController.java
@@ -4,6 +4,7 @@ import com.example.demo.beans.beans.*;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import org.springframework.context.annotation.Scope;
 
 import javax.inject.Provider;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/scope")
 public class BeanController {
